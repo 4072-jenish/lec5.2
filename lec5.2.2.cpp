d59b925e0405496610d5fb959c17166067a52ab4
4072-jenish <150036720+4072-jenish@users.noreply.github.com>
2023-12-29 16:00:51 +0530

    Update lec5.2.2.cpp

--- lec5.2.2.cpp
+++ lec5.2.2.cpp
@@ -1,3 +1,14 @@
+/*Q.2 Write a Program to find the maximum number from the given 4 numbers using nested if else. Also, draw a Flowchart in your book.
+For example,
+Input:
+Enter a value of the first number: 8
+Enter a value of the second number: 3
+Enter a value of the third number: 12
+Enter a value of the fourth number: 7
+
+Output:
+The maximum value is: 12
+*/
 #include<stdio.h>
 #include<conio.h>
 void main(){
