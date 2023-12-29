51238b8cfabd99473f871fec08a2692b36180363
4072-jenish <150036720+4072-jenish@users.noreply.github.com>
2023-12-29 16:01:32 +0530

    Update lec5.2.cpp

--- lec5.2.cpp
+++ lec5.2.cpp
@@ -1,3 +1,14 @@
+/*
+Q.1 Write a Program to find the minimum number from the given 3 numbers using nested if else. Also, draw a Flowchart in your book.
+For example,
+Input:
+Enter a value of the first number: 8
+Enter a value of the second number: 3
+Enter a value of the third number: 12
+
+Output:
+The minimum value is: 3
+*/
 #include<stdio.h>
 #include<conio.h>
 void main(){
